82d5fa3b1d4611fedafb5154d7b90ae59714fca2
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-02-11 12:47:23 +0800

    Clean up code

--- src/persisters/persister-powersync.ts
+++ src/persisters/persister-powersync.ts
@@ -22,8 +22,6 @@ export const createPowerSyncPersister = ((
     configOrStoreTableName,
     async (sql: string, args: any[] = []): Promise<IdObj<any>[]> =>
       await promiseNew(async (resolve, reject) => {
-        // eslint-disable-next-line no-console
-        // console.log('sql/args', sql, args);
         const result = await powerSync.execute(sql, args);
 
         result?.rows ? resolve(result.rows._array) : reject();
@@ -33,55 +31,16 @@ export const createPowerSyncPersister = ((
 
       (async () => {
         for await (const update of powerSync.onChange({
-          // tables: ['profiles'],
           rawTableNames: true,
           signal: abortController.signal,
         })) {
-          // console.log('ONCHANGE2', update);
           arrayForEach(update.changedTables, (tableName) =>
             listener(tableName),
           );
-          // for (const tableName of update.changedTables) {
-          //   listener(tableName);
-          // }
         }
       })();
 
       return abortController;
-
-      // const observer = db.onChange();
-
-      // arrayForEach(powerSync.onChange(), (change) => {
-      //   // eslint-disable-next-line no-console
-      //   console.log(change);
-      // });
-
-      // const listen = async () => {
-      //   for await (const event of powerSync.onChange()) {
-      //     arrayForEach(event.changedTables, (tableName) => {
-      //       // eslint-disable-next-line no-console
-      //       console.log('changedTable', tableName);
-      //     });
-      //   }
-      // };
-
-      // eslint-disable-next-line no-console
-      // console.log('listening!');
-
-      // listen();
-
-      // const listen = async () => {
-      //   for await (const event of powerSync.onChange()) {
-      //     for (const tableName of event.changedTables) {
-      //       observer(null, null, tableName);
-      //     }
-      //   }
-      // };
-
-      // listen();
-
-      // db.on(CHANGE, observer);
-      // return observer;
     },
     (abortController: AbortController) => abortController.abort(),
     onSqlCommand,
